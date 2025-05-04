e2d41f89afc55ba4dc062a2ef0180c54cea31013
Juan Sebastian Garcia <juansgperez@gmail.com>
2025-05-04 12:13:51 -0500

    Productos actualizados con descripción

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -13,30 +13,35 @@ export class LandingComponent {
     {
       id: '1',
       name: 'Allstarz Sunglasses',
+      description: 'Stylish sunglasses for all occasions.',
       price: 49.99,
       image: 'https://www.crtz.xyz/cdn/shop/files/Sunglasses_Yellow_02.png?v=1726478831'
     },
     {
       id: '2',
       name: 'Allstarz Socks',
+      description: 'Stylish sunglasses for all occasions.',
       price: 29.99,
       image: 'https://www.crtz.xyz/cdn/shop/products/image_8d6da066-54b2-4388-927b-d5e1a5da5cec.png?v=1679066389'
     },
     {
       id: '3',
       name: 'Allstarz Contrast Hoodie',
+      description: 'Stylish sunglasses for all occasions.',
       price: 29.99,
       image: 'https://www.crtz.xyz/cdn/shop/files/95RTWContstrastHoodie_BlackYellow_01.png?v=1741944335'
     },
     {
       id: '4',
       name: 'Premium Allstarz Trucker Cap',
+      description: 'Stylish sunglasses for all occasions.',
       price: 29.99,
       image: 'https://www.crtz.xyz/cdn/shop/files/CRTZTruckerCap_BlackYellow_01_1.png?v=1702917901'
     },
     {
       id: '4',
       name: 'Nike Allstarz',
+      description: 'Stylish sunglasses for all occasions.',
       price: 29.99,
       image: 'https://www.crtz.xyz/cdn/shop/files/FB2709-003_400730681_D_E_1X1_7b4d2a11-dfc1-455a-b02b-a682a1b6237f.png?v=1743515541'
     }
